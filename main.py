924e86d6abbde97420c40ba98871782e527eff2b
vas g <vas.g@me.com>
2023-11-25 23:26:20 +0400

    wip

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from core.config import settings
 from core.middlewares.auth_middleware import AuthMiddleware
 from core.middlewares.session_middleware import SessionMiddleware
 from core.middlewares.updates_dumper_middleware import UpdatesDumperMiddleware
-from core.resources.notify_admin import on_shutdown_notify, on_startup_notify
+# from core.resources.notify_admin import on_shutdown_notify, on_startup_notify
 from core.handlers.base_handlers import router as base_router
 from core.handlers.errors_handler import router as errors_router
+from core.handlers.lesson_handlers import router as lesson_router
 
 
 async def main():
@@ -21,9 +22,9 @@ async def main():
     dispatcher.message.middleware(AuthMiddleware())
     dispatcher.callback_query.middleware(AuthMiddleware())
     dispatcher.update.outer_middleware(UpdatesDumperMiddleware())
-    dispatcher.startup.register(on_startup_notify)
-    dispatcher.shutdown.register(on_shutdown_notify)
-    dispatcher.include_routers(base_router, errors_router)
+    # dispatcher.startup.register(on_startup_notify)
+    # dispatcher.shutdown.register(on_shutdown_notify)
+    dispatcher.include_routers(base_router, errors_router, lesson_router)
 
     await dispatcher.start_polling(bot)
 
