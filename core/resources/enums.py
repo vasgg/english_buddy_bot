598e6007c6eb07959b5d0abcdd1f94c60c184cbd
vas g <vas.g@me.com>
2023-11-27 02:25:40 +0400

    working on enums

--- core/resources/enums.py
+++ core/resources/enums.py
@@ -2,12 +2,20 @@ from enum import Enum
 
 
 class SlideType(Enum):
-    INFO = 'info'
+    TEXT = 'text'
+    IMAGE = 'image'
+    STICKER = 'sticker'
     QUIZ_OPTIONS = 'quiz_options'
-    QUIZ_INPUT = 'quiz_input'
+    QUIZ_INPUT_WORD = 'quiz_input_word'
+    QUIZ_INPUT_PHRASE = 'quiz_input_phrase'
 
 
 class LessonLevel(Enum):
     BEGINNER = 'beginner'
     INTERMEDIATE = 'intermediate'
     ADVANCED = 'advanced'
+
+
+class KeyboardType(Enum):
+    FURTHER = 'further'
+    QUIZ = 'quiz'
