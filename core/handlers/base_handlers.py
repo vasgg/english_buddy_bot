67ef7371a2e540d7ad7f52193f9a69f42fdc9a3f
vas g <vas.g@me.com>
2023-11-27 02:23:31 +0400

    working on handlers

--- core/handlers/base_handlers.py
+++ core/handlers/base_handlers.py
@@ -1,26 +1,17 @@
 from aiogram import F, Router, types
 from aiogram.filters import CommandStart
 
-from core.keyboards.base_keyboards import lesson_picker, start_keyboard
+from core.keyboards.base_keyboards import lesson_picker
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def start_message(message: types.Message) -> None:
-    await message.answer(text='<b>Вас приветствует <i>поли-бот</i>!</b>\n'
-                              'Горячие кнопки:\n\n'
-                              '📔 <b>lessons</b> — посмотреть доступные уроки в рамках выбранного уровня знания английского\n'
-                              '📖 <b>start lesson from beginning</b> — начать текущий урок с начала',
-                         reply_markup=start_keyboard)
+    await message.answer(text='<b>Вас приветствует <i>поли-бот</i>!</b>\n',
+                         reply_markup=lesson_picker)
 
 # TODO: вынести текст в переменные
-# TODO: заменить кнопки на выбор урока сразу
-@router.message(F.text == '📔 lessons')
-async def lessons_button_handler(message: types.Message) -> None:
-    await message.answer(text='Выберите урок', reply_markup=lesson_picker)
 
 
-@router.message(F.text == '📖 start lesson from beginning')
-async def start_lesson_from_beginning_button_handler(message: types.Message) -> None:
-    ...
+
--- core/handlers/lesson_handlers.py
+++ core/handlers/lesson_handlers.py
@@ -2,28 +2,35 @@ from aiogram import F, Router, types
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.controllers.lesson_controllers import get_lesson_slide_amount, lesson_routine
-from core.controllers.user_controllers import check_user_progress
+from core.controllers.user_controllers import get_user_progress
 from core.database.models import User
+from core.keyboards.callback_builders import LessonsCallbackFactory, SlideCallbackFactory
 
 router = Router()
 
 
-@router.callback_query(F.data.startswith('lesson_'))
-async def lesson_callback_processing(callback: types.CallbackQuery, user: User, session: AsyncSession) -> None:
-    lesson_number = int(callback.data.split('_')[1])
-    progress = await check_user_progress(user_id=user.id, lesson_number=lesson_number, session=session)
+async def common_callback_processing(callback: types.CallbackQuery, user: User, lesson_number: int, slide_number: int, session: AsyncSession) -> None:
+    progress = await get_user_progress(user_id=user.id, lesson_number=lesson_number, slide_number=slide_number, session=session)
     slide_amount = await get_lesson_slide_amount(lesson_number=lesson_number, session=session)
-    await callback.message.answer(text=f'lesson: {lesson_number} total slides: {slide_amount}, current slide: {progress}')
     await lesson_routine(callback=callback, lesson_number=lesson_number, current_slide=progress, slide_amount=slide_amount, session=session)
     await callback.answer()
 
 
-@router.callback_query(F.data.startswith('next_slide_'))
-async def next_slide_callback_processing(callback: types.CallbackQuery, user: User, session: AsyncSession) -> None:
-    slide_number = int(callback.data.split('_')[2])
-    # progress = await check_user_progress(user_id=user.id, lesson_number=lesson_number, session=session)
-    # if progress < c
-    # slide_amount = await get_lesson_slide_amount(lesson_number=lesson_number, session=session)
-    # await callback.message.answer(text=f'lesson: {lesson_number} total slides: {slide_amount}, current slide: {progress}')
-    # await lesson_routine(callback=callback, lesson_number=lesson_number, current_slide=progress, slide_amount=slide_amount, session=session)
-    # await callback.answer()
+@router.callback_query(LessonsCallbackFactory.filter())
+async def lesson_callback_processing(callback: types.CallbackQuery,
+                                     callback_data: LessonsCallbackFactory,
+                                     user: User,
+                                     session: AsyncSession) -> None:
+    await common_callback_processing(callback=callback, user=user, lesson_number=callback_data.lesson_number, slide_number=1, session=session)
+
+
+@router.callback_query(SlideCallbackFactory.filter())
+async def slide_callback_processing(callback: types.CallbackQuery,
+                                    callback_data: SlideCallbackFactory,
+                                    user: User,
+                                    session: AsyncSession) -> None:
+    lesson_number = callback_data.lesson_number
+    slide_number = callback_data.slide_number
+    await common_callback_processing(callback=callback, user=user, lesson_number=lesson_number, slide_number=slide_number, session=session)
+
+
