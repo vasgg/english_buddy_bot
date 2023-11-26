b602ff0d5668a489bcbd497dac713410230fbe93
vas g <vas.g@me.com>
2023-11-27 02:25:27 +0400

    working on tests

--- tests/test_user_controllers.py
+++ tests/test_user_controllers.py
@@ -1,13 +1,13 @@
 from unittest import IsolatedAsyncioTestCase
 
-from core.controllers.user_controllers import check_user_progress
+from core.controllers.user_controllers import get_user_progress
 from core.database.db import db
 
 
 class Test(IsolatedAsyncioTestCase):
     async def test_check_user_progress(self):
         async with db.session_factory.begin() as session:
-            progress = await check_user_progress(user_id=1, lesson_number=1, session=session)
+            progress = await get_user_progress(user_id=1, lesson_number=1, session=session)
         print(progress)
         # self.fail()
         await db.engine.dispose()
