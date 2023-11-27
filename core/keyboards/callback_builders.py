7b07eaa42e167740e6a7c1e111e764ac7f20a1f1
vas g <vas.g@me.com>
2023-11-28 01:11:30 +0400

    working on keyboards

--- core/keyboards/callback_builders.py
+++ core/keyboards/callback_builders.py
@@ -9,3 +9,8 @@ class SlideCallbackFactory(CallbackData, prefix='slide'):
     lesson_number: int
     slide_number: int
 
+
+class QuizCallbackFactory(CallbackData, prefix='quiz'):
+    lesson_number: int
+    slide_number: int
+    answer: str
