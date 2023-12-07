688057abdf943df974899f53c99737d4daccb937
vas g <vas.g@me.com>
2023-12-08 02:18:33 +0400

    working on session model

--- core/database/models/session.py
+++ core/database/models/session.py
@@ -7,11 +7,9 @@ from core.resources.enums import SessionStatus, SessonStartsFrom
 
 class Session(Base):
     __tablename__ = "sessions"
-    # TODO: add constraint for unique user_id, lesson_id where status = IN PROGRESS
-    # __table_args__ = (UniqueConstraint('user_id', 'current_lesson'), )
 
     lesson_id: Mapped[int] = mapped_column(ForeignKey("lessons.id"))
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     current_slide_id: Mapped[int] = mapped_column(ForeignKey("slides.id"))
-    starts_from: Mapped[SessonStartsFrom] = mapped_column(default=SessonStartsFrom.BEGIN)
+    starts_from: Mapped[SessonStartsFrom] = mapped_column()
     status: Mapped[SessionStatus] = mapped_column(default=SessionStatus.IN_PROGRESS)
