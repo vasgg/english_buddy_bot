ad510c917aed4ced8fae596f4c1aceda588f743d
niko-on-telegram <nikoontelegram@gmail.com>
2023-12-10 13:07:58 +0200

    structure refactoring

--- core/resources/commands.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot, types
-
-
-async def set_bot_commands(bot: Bot) -> None:
-    bot_commands = [
-        types.BotCommand(command="/start", description="start bot"),
-        types.BotCommand(command="/position", description="set slide position"),
-    ]
-    await bot.set_my_commands(bot_commands)
