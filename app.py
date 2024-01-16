666e5dfae885fe17ab4498ceee77c009caf32c30
vas g <vas.g@me.com>
2024-01-16 13:45:18 +0400

    working on slide page

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import logging
 from pathlib import Path
 
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import FastAPI, Form, HTTPException, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import select
 
 from bot.database.db import db
+from bot.database.models.lesson import Lesson
 from bot.database.models.slide import Slide
 from bot.database.models.text import Text
 
@@ -20,34 +21,40 @@ async def read_root():
     return HTMLResponse(content=html_content)
 
 
-# @app.get("/texts", response_class=JSONResponse)
-# async def get_texts():
-#     try:
-#         async with db.session_factory.begin() as db_session:
-#             result = await db_session.execute(select(Text))
-#             data = result.scalars().all()
-#             return data
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.get("/slides", response_class=JSONResponse)
-async def get_texts():
-    try:
-        async with db.session_factory.begin() as db_session:
-            result = await db_session.execute(select(Slide))
-            data = result.scalars().all()
-            return data
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
 @app.get("/texts", response_class=HTMLResponse)
 async def read_texts(request: Request):
     async with db.session_factory.begin() as db_session:
         result = await db_session.execute(select(Text))
         data = result.scalars().all()
     return templates.TemplateResponse(request=request, name="texts.html", context={'texts': data})
+
+
+@app.get("/lessons")
+async def show_lessons(request: Request):
+    async with db.session_factory.begin() as db_session:
+        result = await db_session.execute(select(Lesson))
+        data = result.scalars().all()
+    return templates.TemplateResponse(request=request, name="lessons.html", context={'lessons': data})
+
+
+@app.get("/edit-lesson/{lesson_id}")
+async def edit_lesson(lesson_id: int, request: Request):
+    async with db.session_factory.begin() as db_session:
+        result = await db_session.execute(select(Lesson).where(Lesson.id == lesson_id))
+        data = result.scalars().first()
+    if data is None:
+        raise HTTPException(status_code=404, detail="Lesson not found")
+    return templates.TemplateResponse("lesson.html", {"request": request, "lesson": data})
+
+
+@app.get("/slides/{lesson_id}/{slide_id}")
+async def edit_slide(lesson_id: int, slide_id: int, request: Request):
+    async with db.session_factory.begin() as db_session:
+        result = await db_session.execute(select(Slide).where(Lesson.id == lesson_id, Slide.id == slide_id))
+        data = result.scalars().first()
+    if data is None:
+        raise HTTPException(status_code=404, detail="Slide not found")
+    return templates.TemplateResponse("slide.html", {"request": request, "slide": data, "lesson_id": lesson_id})
 
 
 @app.post("/save-texts")
@@ -65,6 +72,36 @@ async def save_texts(request: Request):
                     raise HTTPException(status_code=404, detail=f"Text with prompt '{key}' not found")
             await db_session.commit()
         return {"message": "Texts updated successfully"}
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+@app.post("/update-lesson/{lesson_id}")
+async def update_lesson(
+    lesson_id: int,
+    first_slide_id: str = Form(...),
+    exam_slide_id: str = Form(...),
+    is_paid: bool = Form(...),
+    total_slides: int = Form(...),
+):
+    try:
+        async with db.session_factory.begin() as db_session:
+            stmt = select(Lesson).where(Lesson.id == lesson_id)
+            result = await db_session.execute(stmt)
+            lesson = result.scalar_one_or_none()
+
+            if not lesson:
+                raise HTTPException(status_code=404, detail="Lesson not found")
+
+            lesson.first_slide_id = first_slide_id
+            lesson.exam_slide_id = exam_slide_id
+            lesson.is_paid = is_paid
+            lesson.total_slides = total_slides
+
+            await db_session.commit()
+
+            return {"message": "Lesson updated successfully"}
     except Exception as e:
         logging.error(f"An error occurred: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
