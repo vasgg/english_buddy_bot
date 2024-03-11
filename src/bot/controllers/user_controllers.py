a2761d0a856b9f578e488e926ba99bbd2fb5b827
vas g <vas.g@me.com>
2024-03-11 18:39:08 +0400

    daily reminders now turn on

--- src/bot/controllers/user_controllers.py
+++ src/bot/controllers/user_controllers.py
@@ -11,6 +11,7 @@ from database.crud.lesson import get_completed_lessons, get_lessons
 from database.crud.user import get_all_users_with_reminders, update_last_reminded_at
 from database.database_connector import DatabaseConnector
 from database.models.user import User
+from enums import Times
 
 
 async def propose_reminder_to_user(message: types.Message, db_session: AsyncSession) -> None:
@@ -31,19 +32,17 @@ async def show_start_menu(user: User, message: types.Message, db_session: AsyncS
 
 async def check_user_reminders(bot: Bot, db_connector: DatabaseConnector):
     while True:
-        await asyncio.sleep(10)
-        # await asyncio.sleep(Times.ONE_HOUR.value)
+        await asyncio.sleep(Times.ONE_HOUR.value)
         utcnow = datetime.utcnow()
-        # if utcnow.hour == Times.UTC_STARTING_MARK.value:
-        async with db_connector.session_factory() as session:
-            for user in await get_all_users_with_reminders(session):
-                delta = utcnow - user.last_reminded_at
-                if delta > timedelta(seconds=user.reminder_freq * 10):
-                    logging.info(f"{'=' * 10} {'reminder sended to ' + str(user)}")
-
-                    await bot.send_message(
-                        chat_id=user.telegram_id,
-                        text=await get_text_by_prompt(prompt='reminder_text', db_session=session),
-                    )
-                    await update_last_reminded_at(user_id=user.id, timestamp=utcnow, db_session=session)
-                    await session.commit()
+        if utcnow.hour == Times.UTC_STARTING_MARK.value:
+            async with db_connector.session_factory() as session:
+                for user in await get_all_users_with_reminders(session):
+                    delta = utcnow - user.last_reminded_at
+                    if delta > timedelta(days=user.reminder_freq):
+                        await bot.send_message(
+                            chat_id=user.telegram_id,
+                            text=await get_text_by_prompt(prompt='reminder_text', db_session=session),
+                        )
+                        logging.info(f"{'=' * 10} {'reminder sended to ' + str(user)}")
+                        await update_last_reminded_at(user_id=user.id, timestamp=utcnow, db_session=session)
+                        await session.commit()
