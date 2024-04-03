d5d8418ed03322773f7a7643d28fcb593e24f6fd
vas g <vas.g@me.com>
2024-04-03 17:54:24 +0400

    get rid of completed lesson table

--- src/database/tables_helper.py
+++ src/database/tables_helper.py
@@ -1,36 +1,26 @@
 from datetime import datetime
 
+from sqlalchemy.ext.asyncio import AsyncEngine
+
 from config import get_settings
 from database.database_connector import DatabaseConnector
 from database.models.base import Base
-
-# noinspection PyUnresolvedReferences
-import database.models.complete_lesson
-
 # noinspection PyUnresolvedReferences
 from database.models.lesson import Lesson
-
 # noinspection PyUnresolvedReferences
 import database.models.quiz_answer_log
-
 # noinspection PyUnresolvedReferences
 from database.models.reaction import Reaction, ReactionType
-
 # noinspection PyUnresolvedReferences
 import database.models.session
-
 # noinspection PyUnresolvedReferences
 from database.models.slide import Slide, SlideType
-
 # noinspection PyUnresolvedReferences
 import database.models.sticker
-
 # noinspection PyUnresolvedReferences
 import database.models.text
-
 # noinspection PyUnresolvedReferences
 from database.models.user import User
-from sqlalchemy.ext.asyncio import AsyncEngine
 
 
 async def create_or_drop_db(engine: AsyncEngine, create: bool = True):
