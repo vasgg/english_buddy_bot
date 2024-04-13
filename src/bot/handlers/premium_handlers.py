a862715d8e2d294185b75cd583b47c20be2f58cf
vas g <vas.g@me.com>
2024-04-14 03:06:39 +0400

    add markup to payment validation message

--- src/bot/handlers/premium_handlers.py
+++ src/bot/handlers/premium_handlers.py
@@ -4,9 +4,10 @@ from aiogram import Router, types
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from bot.keyboards.callback_data import PaymentSentCallbackFactory, PremiumSubCallbackFactory
-from bot.keyboards.keyboards import get_payment_sent_keyboard
+from bot.keyboards.keyboards import get_lesson_picker_keyboard, get_payment_sent_keyboard
 from config import get_settings
 from database.crud.answer import get_text_by_prompt
+from database.crud.lesson import get_completed_lessons_from_sessions, get_lessons
 from database.models.user import User
 from enums import SubscriptionType
 
@@ -42,8 +43,11 @@ async def premium_payment_sent_message(
 ) -> None:
     await callback.answer()
     await callback.message.delete_reply_markup()
+    lessons = await get_lessons(db_session)
+    completed_lessons = await get_completed_lessons_from_sessions(user_id=user.id, db_session=db_session)
     await callback.message.answer(
         text=await get_text_by_prompt(prompt='validating_process_text', db_session=db_session),
+        reply_markup=get_lesson_picker_keyboard(lessons=lessons, completed_lessons=completed_lessons),
     )
     admin_message_text = await get_text_by_prompt(prompt='new_sub_admin_notify', db_session=db_session)
     user_info = f'{user.fullname} (@{user.username})' if user.username else user.fullname
