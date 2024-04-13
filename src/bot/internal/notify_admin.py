74f07338f145cc60e6770361e2a6e8dfc6efce96
vas g <vas.g@me.com>
2024-04-14 03:05:51 +0400

    optimizing bot name retrieving

--- src/bot/internal/notify_admin.py
+++ src/bot/internal/notify_admin.py
@@ -1,21 +1,21 @@
-import os
-
 from aiogram import Bot
 
 from config import get_settings
 
 
 async def on_startup_notify(bot: Bot):
+    bot_info = await bot.get_me()
     await bot.send_message(
         get_settings().ADMIN,
-        f'{os.getcwd().split(os.sep)[-1].capitalize()} started\n\n/start',
+        f'{bot_info.username} started\n\n/start',
         disable_notification=True,
     )
 
 
 async def on_shutdown_notify(bot: Bot):
+    bot_info = await bot.get_me()
     await bot.send_message(
         get_settings().ADMIN,
-        f'{os.getcwd().split(os.sep)[-1].capitalize()} shutdown',
+        f'{bot_info.username} shutdown',
         disable_notification=True,
     )
