14633d536f382f7d1edd430cc98701129dda4519
vas g <vas.g@me.com>
2024-04-23 13:51:00 +0400

    add set of reactions

--- src/database/crud/answer.py
+++ src/database/crud/answer.py
@@ -6,11 +6,18 @@ from database.models.sticker import Sticker
 from database.models.text import Text
 from enums import ReactionType, StickerType
 
+reaction_dict = {
+    ReactionType.RIGHT: set(),
+    ReactionType.WRONG: set(),
+}
+
 
 async def get_random_answer(mode: ReactionType, db_session: AsyncSession) -> str:
-    query = select(Reaction.text).filter(Reaction.type == mode).order_by(func.random()).limit(1)
-    result = await db_session.execute(query)
-    return result.scalar()
+    if not reaction_dict[mode]:
+        query = select(Reaction.text).filter(Reaction.type == mode)
+        result = await db_session.execute(query)
+        reaction_dict[mode].update(result.scalars().all())
+    return reaction_dict[mode].pop()
 
 
 async def get_random_sticker_id(mode: StickerType, db_session: AsyncSession) -> str:
