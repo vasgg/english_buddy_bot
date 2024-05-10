01121bad339bcd703f89d5331c9bec9de8d1da2e
vas g <vas.g@me.com>
2024-05-10 18:11:37 +0400

    add test for garbage collector

---
+++ tests/database_tests/test_garbage_collector.py
@@ -0,0 +1,17 @@
+from database.database_connector import DatabaseConnector
+from database.garbage_helper import collect_garbage
+from database.models.lesson import Lesson
+from database.models.slide import Slide
+from enums import LessonStatus, SlideType
+
+
+async def test_garbage_collector(db: 'DatabaseConnector'):
+    slide_id = 1
+    path = f"1.{slide_id}"
+
+    async with db.session_factory.begin() as session:
+        session.add(Lesson(title="abacaba", path=path, is_active=LessonStatus.DISABLED))
+        session.add(Slide(lesson_id=1, slide_type=SlideType.TEXT, id=slide_id))
+
+    async with db.session_factory.begin() as session:
+        await collect_garbage(session)
