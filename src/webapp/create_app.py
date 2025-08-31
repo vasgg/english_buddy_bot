64bceef9d59a5e28e89af1b1061aa8bf17c4477e
vasgg <vas.g@me.com>
2025-08-31 15:45:05 +0400

    working on webapp

--- src/webapp/create_app.py
+++ src/webapp/create_app.py
@@ -2,22 +2,52 @@ from os.path import dirname, exists, join, realpath
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
-from webapp.routers.lessons import router as lessons_fastui_router
-from webapp.routers.newsletter import router as newsletter_fastui_router
-from webapp.routers.reactions import router as reaction_fastui_router
-from webapp.routers.root import router as root_fastui_router
-from webapp.routers.slides_get import router as slides_get_fastui_router
-from webapp.routers.slides_post import router as slides_post_fastui_router
-from webapp.routers.statistics import router as statistics_fastui_router
-from webapp.routers.texts import router as texts_fastui_router
-from webapp.routers.users import router as users_fastui_router
+from fastui import AnyComponent
+from fastui import components as c
+from fastui.components.display import DisplayLookup
+from fastui import FastUI as _FastUI
 
 static_files_path = join(dirname(realpath(__file__)), 'static')
 assert exists(static_files_path), static_files_path
 
 
 def create_app():
+    c.AnyComponent = AnyComponent  # type: ignore[attr-defined]
+    for model in (
+        c.ModelForm,
+        c.Table,
+        c.PageTitle,
+        c.Navbar,
+        c.Page,
+        c.Paragraph,
+        c.Heading,
+        c.Button,
+        c.Text,
+        c.Link,
+    ):
+        try:
+            model.model_rebuild()
+        except Exception:
+            pass
+    try:
+        DisplayLookup.model_rebuild()
+    except Exception:
+        pass
+    try:
+        _FastUI.model_rebuild()
+    except Exception:
+        pass
+
+    from webapp.routers.lessons import router as lessons_fastui_router
+    from webapp.routers.newsletter import router as newsletter_fastui_router
+    from webapp.routers.reactions import router as reaction_fastui_router
+    from webapp.routers.root import router as root_fastui_router
+    from webapp.routers.slides_get import router as slides_get_fastui_router
+    from webapp.routers.slides_post import router as slides_post_fastui_router
+    from webapp.routers.statistics import router as statistics_fastui_router
+    from webapp.routers.texts import router as texts_fastui_router
+    from webapp.routers.users import router as users_fastui_router
+
     app = FastAPI()
     app.mount("/static", StaticFiles(directory=static_files_path), name="static")
     app.include_router(slides_get_fastui_router, prefix="/api/slides")
